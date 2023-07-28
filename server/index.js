e282bb95ceabb8b8a6767fa33882cb73a9a6409a
Vk4011 <maticsystem1@gmail.com>
2023-07-28 22:38:18 +0530

    server

--- server/index.js
+++ server/index.js
@@ -1,44 +1,36 @@
-
 const fs = require("fs");
 const express = require("express");
 
-const  app = express();
-const port = 4000
+const app = express();
+const port = 4000;
 
-
-function handleFirstRequest(req,res){
-    res.send('Hello World!')
+function handleFirstRequest(req, res) {
+  var ans = "The calculated sum : "+Sum(100);
+  console.log("\n\t sum of n natural numbers", ans, "\n");
+  res.send(ans);
 }
 
-app.get('/',handleFirstRequest)
+app.get("/", handleFirstRequest);
 
-
-function started(){
-    console.log(`\n\t Example app listening on port ${port}`);
+function started() {
+  console.log(`\n\t Example app listening on port ${port}`);
 }
 
-app.listen(port, started)
-    
+app.listen(port, started);
 
-
-
-
-function callbackFn(err,data){
-    console.log(data);
+function callbackFn(err, data) {
+  console.log(data);
 }
 
+fs.readFile("a.txt", "utf-8", callbackFn);
 
-fs.readFile("a.txt","utf-8",callbackFn);
-
-
-
-function Sum(counter){
-    var sum=0;
-    for(i=0; i<=counter ; i++){
-        sum = sum+i;
-    }
-    return sum;
+function Sum(counter) {
+  var sum = 0;
+  for (i = 0; i <= counter; i++) {
+    sum = sum + i;
+  }
+  return sum;
 }
 
 var ans = Sum(100);
-console.log("\n\t sum of n natural numbers",ans,"\n")
+console.log("\n\t sum of n natural numbers", ans, "\n");
