37878b89e50810078d4fb06dd018b758a35cc920
Vk4011 <maticsystem1@gmail.com>
2023-08-04 21:34:49 +0530

    scope

---
+++ Modules/wraper.js
@@ -0,0 +1,9 @@
+
+(function(message){
+    const superHero = "Batman";
+    console.log(message,superHero);
+})("\n\t Hello");
+(function(message){
+    const superHero = "Superman";
+    console.log(superHero);
+})("\n\t Hey");
