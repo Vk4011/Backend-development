31353ab1cb447cf9f81a987903cd131f23a4fb74
Vk4011 <maticsystem1@gmail.com>
2023-08-05 22:10:22 +0530

    caching class

---
+++ Modules/Modulecaching.js
@@ -0,0 +1,8 @@
+const caching = require("./caching");
+console.log(caching.getName());
+caching.setName("Superman");
+console.log(caching.getName());
+
+
+const newSuperHero = require("./caching");
+console.log(newSuperHero.getName());
