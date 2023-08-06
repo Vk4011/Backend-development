5797f280da37b8033ec3f022266ffdf3cfe87a86
Vk4011 <maticsystem1@gmail.com>
2023-08-06 21:15:04 +0530

    object destructuring

--- Modules/import.js
+++ Modules/import.js
@@ -1,3 +1,10 @@
 const math = require("./exports")
 console.log(math.add(2,3));
 console.log(math.subtract(2,3));
+console.log("\n\t After destruring")
+
+//Destructuring
+
+const {add,subtract} = math;
+console.log(add(4,5));
+console.log(subtract(5,4))
