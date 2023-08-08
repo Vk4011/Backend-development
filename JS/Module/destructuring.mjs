aa20abd5149e732f2fe84b6d3c47e91ff7c23a99
Vk4011 <maticsystem1@gmail.com>
2023-08-08 07:19:53 +0530

    destructuring from import

--- JS/Module/destructuring.mjs
+++ JS/Module/destructuring.mjs
@@ -3,3 +3,4 @@ import m from "./Es.mjs";
 const {add,subtract} = m;
 console.log(add(5,5));
 console.log(subtract(9,8));
+
---
+++ JS/Module/import.mjs
@@ -0,0 +1,4 @@
+import {add,subtract} from "./math.mjs";
+
+console.log(add(9,7));
+console.log(subtract(9,7));
