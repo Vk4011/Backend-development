9249351b7da849f25c9e8c94664476763e7acab3
Vk4011 <maticsystem1@gmail.com>
2023-08-10 22:13:49 +0530

    Built in module

---
+++ Built/path/index.js
@@ -0,0 +1,19 @@
+const path = require("node:path");
+
+console.log(__filename);
+console.log(__dirname);
+
+
+console.log(path.basename(__filename));
+console.log(path.basename(__dirname));
+
+console.log(path.basename(__filename));
+console.log(path.basename(__dirname));
+
+
+console.log(path.parse(__filename));
+console.log(path.parse(__filename));
+console.log(path.isAbsolute(__filename));
+console.log(path.isAbsolute("./data.json"));
+
+
