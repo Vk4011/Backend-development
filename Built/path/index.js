af274608784a8fe7f2386b68a000159265eae7e3
Vk4011 <maticsystem1@gmail.com>
2023-08-11 11:03:08 +0530

    path

--- Built/path/index.js
+++ Built/path/index.js
@@ -7,12 +7,13 @@ console.log(__dirname);
 console.log(path.basename(__filename));
 console.log(path.basename(__dirname));
 
-console.log(path.basename(__filename));
-console.log(path.basename(__dirname));
+console.log(path.extname(__filename));
+console.log(path.extname(__dirname));
 
 
 console.log(path.parse(__filename));
-console.log(path.parse(__filename));
+console.log(path.format(path.parse(__filename)));
+
 console.log(path.isAbsolute(__filename));
 console.log(path.isAbsolute("./data.json"));
 
