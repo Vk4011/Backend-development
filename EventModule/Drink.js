8bc233b1126b9d7be064a982a6816e5517743d29
Vk4011 <maticsystem1@gmail.com>
2023-08-14 19:03:53 +0530

    custom event

---
+++ EventModule/Drink.js
@@ -0,0 +1,10 @@
+
+class DrinkMachine{
+    serverDrink(size){
+        if(size === "large"){
+            console.log("Servering complementary drink")
+        }
+    }
+}
+
+module.exports = DrinkMachine;
--- EventModule/shop.js
+++ EventModule/shop.js
@@ -1,8 +1,14 @@
 const PizzaShop =  require("./PizzaShop.mjs");
+const DrinkMachine= require("./Drink");
+
+
 const pizaa = new PizzaShop();
+const drinkMachine = new DrinkMachine();
+
+
 pizaa.on("order", (size,topping)=>{
     console.log("\n\t,"`Order recevied Baking a ${size} pizaa with ${topping}`);
-
+    drinkMachine.serverDrink(size);
 })
 pizaa,order("large","Chesse");
 pizaa.displayOrderNumber();
