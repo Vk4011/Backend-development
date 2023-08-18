9c1dce8e3c5b4a3d6c4f418ff100be6fa9dbd7ee
Vk4011 <maticsystem1@gmail.com>
2023-08-18 06:12:05 +0530

    res.end

--- Http/server.js
+++ Http/server.js
@@ -1,7 +1,15 @@
 
 const http = require("node:http");
 
-http.createServer((req,res)=>{
 
-    
+const server =  http.createServer((req,res)=>{
+    res.writeHead(200)
+    res.end("Hello world!");
+
+
 })
+
+server.listen(3000,()=>{
+    console.log('Server runnig on port 3000')
+});
+
