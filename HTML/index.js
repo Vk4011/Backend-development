c71e8d32931d387a440a2c4af0e19b23b1b7fb40
Vk4011 <maticsystem1@gmail.com>
2023-08-20 13:37:06 +0530

    pipe

--- HTML/index.js
+++ HTML/index.js
@@ -4,10 +4,12 @@ const fs=require("node:fs")
 const server = http.createServer((req,res)=>{
     
     res.writeHead(200,{"Content-Type":"text/html"});
-    const html = fs.readFileSync("./index.html","utf-8");
-    res.end(html);
+    // const html = fs.readFileSync("./index.html","utf-8");
+    //  res.end(html);
+    fs.createReadStream(__dirname+"./index.html").pipe(res);
+   
 })
 
 server.listen(4000,()=>{
-    console.log("\n\t Server running on port 6000 \n");
+    console.log("\n\t Server running on port 4000 \n");
 })
