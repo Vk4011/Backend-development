17803455eb0d8374291de02c414f078c29975e1e
Vk4011 <maticsystem1@gmail.com>
2023-08-21 10:34:35 +0530

    replace

--- Html Template/index.js
+++ Html Template/index.js
@@ -2,8 +2,10 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req,res)=>{
+    const name="Vikram";
     res.writeHead(200,{"Content-Type":"text/html"});
-    const html = fs.readFileSync("./index.html","utf-8");
+    let  html = fs.readFileSync("./index.html","utf-8");
+    html = html.replace("{{name}}",name)
     res.end(html);
 })
 
