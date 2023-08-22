620c3b93bbf4f125054798c15f64c81654700ca8
Vk4011 <maticsystem1@gmail.com>
2023-08-22 21:30:41 +0530

    json

--- Routing/index.js
+++ Routing/index.js
@@ -16,7 +16,7 @@ const server = http.createServer((req,res)=>{
         res.end("About Page");
     }   
         else if(req.url === "/api"){
-        res.writeHead(200,{"Content-Type":"aplication/json"})
+        res.writeHead(200,{"Content-Type":"application/json"})
         res.end(JSON.stringify({
             firstName : "Bruce",
             lastName: "Wayne"
