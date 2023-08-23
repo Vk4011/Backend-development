73a5f92becb50d38a64a91edd17e15a0dc49d86d
Vk4011 <maticsystem1@gmail.com>
2023-08-23 21:24:54 +0530

    pbkdf2

---
+++ WEB/server.js
@@ -0,0 +1,7 @@
+const crypto = require("node:crypto");
+
+// crypto.pbkdf2=Date.now();  password based key dirivation function 2 # methods
+
+const start = Date.now();
+crypto.pbkdf2Sync("password","salt",100000,512,"sha512");
+console.log("Hash: ",Date.now()  - start);
