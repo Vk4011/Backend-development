a6b47c8d4671a81a3f123b6c7075919ad4b40669
Vk4011 <maticsystem1@gmail.com>
2023-08-23 21:31:42 +0530

    crypto

---
+++ WEB/threadpool.js
@@ -0,0 +1,14 @@
+
+const fs = require("node:fs");
+const crypto = require("node:crypto")
+
+const MAX_CALLS = 1;
+
+const start = Date.now();
+for( let i=0; i<MAX_CALLS; i++){
+    crypto.pbkdf2("password","salt",10000,512,"sha512",()=>{
+        console.log(`hash: ${i+1}`,Date.now() -start);
+
+    })
+}
+
