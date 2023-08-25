b9f9132fb74d0572fdeee07a8dc6c28b2cdea6a0
Vk4011 <maticsystem1@gmail.com>
2023-08-25 10:30:08 +0530

    Time queue

---
+++ WEB/SetTimeout.js
@@ -0,0 +1,4 @@
+
+setTimeout(()=>console.log("\n\t This setTimeout 1 "),1000);
+setTimeout(()=>console.log("\n\t This setTimeout 2 "),2000);
+setTimeout(()=>console.log("\n\t This setTimeout 3 "),0);
