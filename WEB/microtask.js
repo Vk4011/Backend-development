20737e1faaa6f86c4522b0716811549e9ba8a90b
Vk4011 <maticsystem1@gmail.com>
2023-08-25 09:43:46 +0530

    nextTick

--- WEB/microtask.js
+++ WEB/microtask.js
@@ -1,3 +1,8 @@
 console.log("\n\tconsole.log 1");
 process.nextTick(()=>console.log("\n\t This process.next 1 "))
 console.log("\n\tconsole.log 2");
+
+
+
+Promise.resolve().then(()=> console.log("\n\t This is Promise.resolve 1"));
+process.nextTick(()=> console.log("\n\t This is process.nextTick 1"));
