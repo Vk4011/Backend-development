ff9632b80ce809f3fe5001cd924e1e58042faf6c
Vk4011 <maticsystem1@gmail.com>
2023-08-25 09:52:38 +0530

    Micro task Queues

--- WEB/microtask.js
+++ WEB/microtask.js
@@ -6,3 +6,5 @@ console.log("\n\tconsole.log 2");
 
 Promise.resolve().then(()=> console.log("\n\t This is Promise.resolve 1"));
 process.nextTick(()=> console.log("\n\t This is process.nextTick 1"));
+
+
