801cee1dd946b76c291879042d20da875de9f32c
Vk4011 <maticsystem1@gmail.com>
2023-08-27 22:39:25 +0530

    check queu

--- WEB/IOQueue.js
+++ WEB/IOQueue.js
@@ -2,12 +2,17 @@ const fs = require("fs");
 
 fs.readFile(__filename,()=>{
     console.log("\n\t This readFile 1");
-})
+    setImmediate(()=>console.log("\n\t This is inner setImmediate inside readFile"));
+});
+
 
 process.nextTick(()=>console.log("\n\t This is process.nextTick 1"));
 Promise.resolve().then(()=>console.log("\n\t This is Promise.resolve 1"));
 setTimeout(()=>console.log("\n\t This is setTimeout 1"),0);
+setTimeout(()=>console.log("\n\t This is setImmediate 1"));
+
+
 
 for(let i = 0 ; i < 20000000 ; i++){
-    
+
 }
