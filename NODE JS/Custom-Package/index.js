a221da358c1c4867ba8efa2fb57b5b56dde658d0
Vk4011 <maticsystem1@gmail.com>
2023-08-28 05:08:58 +0530

    Using Packages

--- NODE JS/Custom-Package/index.js
+++ NODE JS/Custom-Package/index.js
@@ -1,8 +1,16 @@
 
+// const localModule = require("./path-to-module");
+
+const upperCase = require("upper-case").upperCase;
+
+
+
 function greet(name) {
-    console.log(`Hello ${name}, welcome to NodeJS`);
+    console.log(upperCase(`Hello ${name}, welcome to NodeJS`));
 
 }
+
+greet("vikam");
 
 module.exports = greet;
 
