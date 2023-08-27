4f0b9cb7e7d14918998a8d777b3fd4e2182cf770
Vk4011 <maticsystem1@gmail.com>
2023-08-27 22:27:12 +0530

    IO polling

---
+++ WEB/IOQueue.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+fs.readFile(__filename,()=>{
+    console.log("\n\t This readFile 1");
+})
+
+process.nextTick(()=>console.log("\n\t This is process.nextTick 1"));
+Promise.resolve().then(()=>console.log("\n\t This is Promise.resolve 1"));
+setTimeout(()=>console.log("\n\t This is setTimeout 1"),0);
+
+for(let i = 0 ; i < 20000000 ; i++){
+    
+}
