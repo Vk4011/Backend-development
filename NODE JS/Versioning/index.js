51681b816247f2f8add04ba15f85c7c99cd927d3
Vk4011 <maticsystem1@gmail.com>
2023-09-01 11:33:44 +0530

    Global packages

---
+++ NODE JS/Versioning/index.js
@@ -0,0 +1,15 @@
+
+
+console.log("\n\t Global packages ...")
+
+function sum( a,b){
+    console.log("\n\t\tNodemon....\n");
+    return a+b;
+}
+var ans=sum(9,10);
+console.log("\n\t Using Nodemon to run sum : "+ans);
+let r=sum(89,100);
+console.log("\n\t it will restart js file automatically  sum :"+r);
+
+
+
