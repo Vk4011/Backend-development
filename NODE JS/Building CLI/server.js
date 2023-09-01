cec850fcb105d6fa48c5f05a2fb62613718662b2
Vk4011 <maticsystem1@gmail.com>
2023-09-01 12:14:10 +0530

    building cli

---
+++ NODE JS/Building CLI/server.js
@@ -0,0 +1,15 @@
+
+// #!/usr/bin/env node
+
+console.log("\n\t Buliding cli\n");
+console.log("\n\t using nodemon and npm script r");
+console.log("\n\t run by using npm run r");
+
+console.log("\n\t npm start will run only node server.js\n")
+
+//npm i -g
+//again install npm i -g
+
+console.log("\n\t npm install will cli tool globaly ")
+
+console.log(" \n\t code-vikram:index.js");
