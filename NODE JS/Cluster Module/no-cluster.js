0b8f633d5595d891348af06b8bf3c51510121263
Vk4011 <maticsystem1@gmail.com>
2023-09-01 12:56:08 +0530

    Cluster Module

---
+++ NODE JS/Cluster Module/no-cluster.js
@@ -0,0 +1,19 @@
+
+const http = require("node:http");
+
+const server = http.createServer((req,res)=>{
+    if(req.url === "/"){
+        res.writeHead(200, { "Content-Type": "text/plain"});
+        res.end("Home page");
+
+    }
+    else if(req.url === "/slow-page"){
+        for(let i =0; i<6000000000;i++){}
+        res.writeHead(200,{"Content-Type":"text/plain"});
+        res.end("Slow Page");
+
+    }
+});
+
+server.listen(8000,()=>console.log("\n\t Server is running on port 8000"));
+
