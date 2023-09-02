d3787477535fb0e225b1ecbf61b516cf0304068e
Vk4011 <maticsystem1@gmail.com>
2023-09-02 20:52:55 +0530

    fork

--- NODE JS/Cluster Module/cluster.js
+++ NODE JS/Cluster Module/cluster.js
@@ -2,7 +2,25 @@
 const cluster = require("node:cluster")
 
 if(cluster.isMaster){
-    console.log(`MAster process ${process.pid} is runnig`);
+    console.log(`Master process ${process.pid} is runnig`);
+    cluster.fork()
+    cluster.fork()
 }else{
     console.log(`Worker ${process.pid} started`);
+    const server = http.createServer((req,res)=>{
+        if(req.url === "/"){
+            res.writeHead(200,{"content-Type":"text/plain"});
+            res.end("Home page");
+        }else if(req.url === "/slow-page"){
+            for(let i =0; i<6000000000;i++){} //Simulate CPU work
+            res.writeHead(200,{"Content-Type":"text/plain"});
+            res.end("Slow Page");
+        }
+    })
 }
+server.listen(8000,()=>console.log("\n\t Server is running on port 8000"));
+//create a large number of workers using cluster.fork()?
+//we should only create as many workers as there are cpu cores on the machine the app is running
+
+//if you create more workers than there are logical cores on the computer it can cause an overhead as the system will have to schedule all the created 
+// Workers with fewer number of cores
