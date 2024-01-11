741db8cf14753ccce14e140a83856733fb80e1e4
Vk4011 <maticsystem1@gmail.com>
2024-01-12 00:27:24 +0530

    boilerplate-express

---
+++ boilerplate-express/myApp.js
@@ -0,0 +1,16 @@
+let express = require("express");
+let app = express();
+console.log("Hello World");
+// app.get("/", (req, res) =>   res.send("Hello Express"));
+
+app.get("/", (req, res) => {
+  const absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
+});
+module.exports = app;
