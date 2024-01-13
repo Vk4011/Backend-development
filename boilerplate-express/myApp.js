db29842053cd29e2a4bc772185c94ad07e0bd4b1
Vk4011 <maticsystem1@gmail.com>
2024-01-13 10:50:30 +0530

    Middlewares

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,7 +1,19 @@
-let express = require("express");
-let app = express();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Logger middleware
+app.use((req, res, next) => {
+  // Log the request method, path, and IP address
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+
+  // Call the next middleware in the stack
+  next();
+});
+
 console.log("Hello World");
-// app.get("/", (req, res) =>   res.send("Hello Express"));
 
 app.get("/", (req, res) => {
   const absolutePath = __dirname + "/views/index.html";
@@ -13,4 +25,27 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", (req, res) => {
   res.json({ message: "Hello json" });
 });
+
+const middleWare = (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+};
+app.get("/now", middleWare, (req, res) => {
+  res.send({ time: req.time });
+});
+
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.send({ echo: word });
+});
+
+app.get("/name", ({ query: { first, last } }, res) => {
+  res.json({ name: `${first} ${last}` });
+});
+
+app.post("/name", (req, res) => {
+  const { first, last } = req.body;
+  res.json({ name: `${first} ${last}` });
+});
+
 module.exports = app;
